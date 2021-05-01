5fb65e0a22add5efd06a7eec2bea6181d54533d8
leojen23 <leojen23@hotmail.com>
2021-05-01 16:29:31 +0200

    test

--- App.js
+++ App.js
@@ -1,39 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 import { Provider } from 'react-redux';
 import store from './store';
 import Forecast from './components/Forecast';
+import { 
+  StyleSheet,
+  View, 
+  ImageBackground
+ } from 'react-native';
+
 import * as Font from 'expo-font';
 import { useFonts } from 'expo-font';
 
 
 export default function App() {
-  const [loaded] = useFonts({
-    Montserrat: require('./assets/fonts/Montserrat-Black.ttf'),
-  });
+  // const [loaded] = useFonts({
+  //   Montserrat: require('./assets/fonts/Montserrat-Black.ttf'),
+  // });
   
-  if (!loaded) {
-    return null;
-  }
+  // if (!loaded) {
+  //   return null;
+  // }
   return (
     <Provider store={store}>
-    {/*   <View style={styles.container}>
-        <Text style={{ fontFamily: 'Montserrat', fontSize: 30, color: 'white' }}> */}
-            <Forecast />
-      {/*   </Text>
-        <StatusBar style="auto" />
-
-      </View> */}
+       <View style={styles.container}>
+        <ImageBackground source={require('./assets/images/Bg_weatherApp.png')} style={styles.image}>
+          <Forecast />
+          <StatusBar style="hidden" />
+        </ImageBackground>
+        </View>
     </Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    /* backgroundColor: '#343a40' , */
-   /*  color: '#fff', */
-/*     alignItems: 'center',
-    justifyContent: 'center', */
+    flex:1,
   },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center"
+  },
+
 });
--- store.js
+++ store.js
@@ -1,22 +1,3 @@
-/* import {combineReducers, createStore, applyMiddleware} from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import forecastReducer from "./reducers/forecastReducer";
-import thunk from 'redux-thunk';
-
-const composeEnhancers = composeWithDevTools({
-    // Specify name here, actionsBlacklist, actionsCreators and other options if needed
-});
-
-const middleware = [
-    thunk
-];
-let store = createStore(combineReducers({forecastReducer}),
-    composeEnhancers(
-        applyMiddleware(...middleware),
-    )
-);
-export default store; */
-
 import { createStore, applyMiddleware } from "redux";
 import thunk from 'redux-thunk';
 import forecastReducer from "./reducers/forecastReducer";
